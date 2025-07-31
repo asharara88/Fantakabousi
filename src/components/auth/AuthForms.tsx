8dfe461377e612025d2e72b24c39c17ef6e3bc38
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-31 14:05:12 +0400

    Updated AuthForms.tsx

--- src/components/auth/AuthForms.tsx
+++ src/components/auth/AuthForms.tsx
@@ -33,3 +33,4 @@ const AuthForms: React.FC = () => {
   const logoUrl = actualTheme === 'dark' 
     ? "https://leznzqfezoofngumpiqf.supabase.co/storage/v1/object/sign/biowelllogos/Biowell_Logo_Dark_Theme.svg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82ZjcyOGVhMS1jMTdjLTQ2MTYtOWFlYS1mZmI3MmEyM2U5Y2EiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJiaW93ZWxsbG9nb3MvQmlvd2VsbF9Mb2dvX0RhcmtfVGhlbWUuc3ZnIiwiaWF0IjoxNzUzNzY4NjI5LCJleHAiOjE3ODUzMDQ2Mjl9.FeAiKuBqhcSos_4d6tToot-wDPXLuRKerv6n0PyLYXI"
     : "https://leznzqfezoofngumpiqf.supabase.co/storage/v1/object/sign/biowelllogos/Biowell_Logo_Light_Theme.svg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82ZjcyOGVhMS1jMTdjLTQ2MTYtOWFlYS1mZmI3MmEyM2U5Y2EiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJiaW93ZWxsbG9nb3MvQmlvd2VsbF9Mb2dvX0xpZ2h0X1RoZW1lLnN2ZyIsImlhdCI6MTc1Mzc2ODYyOSwiZXhwIjoxNzg1MzA0NjI5fQ.8vQZQCJqhcSos_4d6tToot-wDPXLuRKerv6n0PyLYXI";
+}
