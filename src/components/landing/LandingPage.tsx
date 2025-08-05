e0c331257ba5ce3e603202a4b3930e4645ab2a40
asharara88 <ahmed.m.sharara@gmail.com>
2025-08-05 11:54:51 +0400

    Updated LandingPage.tsx

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { useTheme } from '../../contexts/ThemeContext';
 import AuthForms from '../auth/AuthForms';
 import { 
